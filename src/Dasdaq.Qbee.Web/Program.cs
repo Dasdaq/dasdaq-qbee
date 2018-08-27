2d9332650cae55df7056e7014d5926d8d6069444
Yuko Zheng <1@1234.sh>
2018-08-27 20:44:19 +0800

    Add run.sh

--- src/Dasdaq.Qbee.Web/Program.cs
+++ src/Dasdaq.Qbee.Web/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Dasdaq.Qbee.Web
 {
@@ -20,6 +13,7 @@ namespace Dasdaq.Qbee.Web
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*:5503")
                 .Build();
     }
 }
